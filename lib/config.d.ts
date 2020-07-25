5461cf4fb1d47c0d97f8580a5328ca8bfe2f88e3
stasandk <stasandk@yahoo.com>
2020-07-25 14:59:12 +0200

    Lib update

--- lib/config.d.ts
+++ lib/config.d.ts
@@ -1,9 +1,8 @@
 /// <reference types="node" />
 import * as fs from 'fs';
 import debug from 'debug';
-declare const THIS_MIGRATIONS_PATH = "/sql/migrations-table.sql";
 declare const POSTGRES_URL: string | undefined;
 declare const log: debug.Debugger;
 declare const USER_MIGRATIONS_PATH: string;
 declare const fsAsync: typeof fs.promises;
-export { log, THIS_MIGRATIONS_PATH, USER_MIGRATIONS_PATH, fsAsync, POSTGRES_URL };
+export { log, USER_MIGRATIONS_PATH, fsAsync, POSTGRES_URL };
