77833a09d357461344b30263511cf53a3ce6de73
stasandk <stasandk@yahoo.com>
2020-07-29 11:51:44 +0200

    PG ssl fix

--- lib/config.d.ts
+++ lib/config.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="node" />
 import * as fs from 'fs';
 import debug from 'debug';
-declare const DATABASE_URL: string | undefined;
+declare const NODE_ENV: string | undefined, DATABASE_URL: string | undefined;
 declare const log: debug.Debugger;
 declare const USER_MIGRATIONS_PATH: string;
 declare const fsAsync: typeof fs.promises;
-export { log, USER_MIGRATIONS_PATH, fsAsync, DATABASE_URL };
+export { log, USER_MIGRATIONS_PATH, fsAsync, DATABASE_URL, NODE_ENV };
